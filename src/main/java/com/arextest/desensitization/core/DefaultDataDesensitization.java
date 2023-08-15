4bde61d345d81b170a51dd11cf544436ba23fb43
coryhh <jschenrui@163.com>
2023-08-15 15:35:18 +0800

    feat：init core

---
+++ src/main/java/com/arextest/desensitization/core/DefaultDataDesensitization.java
@@ -0,0 +1,15 @@
+package com.arextest.desensitization.core;
+
+import com.arextest.desensitization.extension.DataDesensitization;
+
+public class DefaultDataDesensitization implements DataDesensitization {
+    @Override
+    public String encrypt(String s) throws Exception {
+        return "";
+    }
+
+    @Override
+    public String decrypt(String s) throws Exception {
+        return "";
+    }
+}
