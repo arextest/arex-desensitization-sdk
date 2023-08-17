72aae9dd071d2ccc5e055d2c799f6f2099067005
coryhh <jschenrui@163.com>
2023-08-17 21:07:50 +0800

    feat：init core

--- src/main/java/com/arextest/desensitization/core/DefaultDataDesensitization.java
+++ src/main/java/com/arextest/desensitization/core/DefaultDataDesensitization.java
@@ -1,15 +1,54 @@
 package com.arextest.desensitization.core;
 
+import com.arextest.desensitization.core.utils.AesKeyReaderUtil;
 import com.arextest.desensitization.extension.DataDesensitization;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 
 public class DefaultDataDesensitization implements DataDesensitization {
     @Override
     public String encrypt(String s) throws Exception {
-        return "";
+        if (AesKeyReaderUtil.aesKey == null) {
+            return s;
+        }
+        byte[] contentBytes = s.getBytes();
+        byte[] encryptedContent = encryptAES(contentBytes, AesKeyReaderUtil.aesKey);
+        return Base64.getEncoder().encodeToString(encryptedContent);
     }
 
     @Override
     public String decrypt(String s) throws Exception {
-        return "";
+        if (AesKeyReaderUtil.aesKey == null) {
+            return s;
+        }
+        byte[] base64DecodeContent = Base64.getDecoder().decode(s);
+        byte[] decryptedContent = decryptAES(base64DecodeContent, AesKeyReaderUtil.aesKey);
+        return new String(decryptedContent);
+    }
+
+
+    /**
+     * encrypt
+     *
+     * @throws Exception
+     */
+    private byte[] encryptAES(byte[] data, byte[] key) throws Exception {
+        SecretKey secretKey = new SecretKeySpec(key, "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        return cipher.doFinal(data);
+    }
+
+    /**
+     * decrypt
+     */
+    private byte[] decryptAES(byte[] data, byte[] key) throws Exception {
+        SecretKey secretKey = new SecretKeySpec(key, "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        return cipher.doFinal(data);
     }
 }
--- src/main/java/com/arextest/desensitization/core/utils/AesKeyReaderUtil.java
+++ src/main/java/com/arextest/desensitization/core/utils/AesKeyReaderUtil.java
@@ -1,4 +1,66 @@
 package com.arextest.desensitization.core.utils;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class AesKeyReaderUtil {
+    public static byte[] aesKey = null;
+
+    static {
+        writeAesKey("./aesKey.bin");
+        aesKey = readAesKey("./aesKey.bin");
+        System.out.println("AesKeyReaderUtil init");
+    }
+
+    private static byte[] readAesKey(String keyFilePath) {
+        byte[] keyBytes = null;
+        File file = new File(keyFilePath);
+        if (file.exists()) {
+            try {
+                FileInputStream fis = new FileInputStream(keyFilePath);
+                keyBytes = new byte[(int) fis.available()];
+                fis.read(keyBytes);
+                fis.close();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return keyBytes;
+    }
+
+    private static void writeAesKey(String keyFilePath) {
+        try {
+            // 生成AES密钥
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+            keyGenerator.init(256);  // 可以根据需要调整密钥长度
+            SecretKey secretKey = keyGenerator.generateKey();
+            byte[] keyBytes = secretKey.getEncoded();
+
+            // 将密钥写入文件
+            FileOutputStream fos = new FileOutputStream(keyFilePath);
+            fos.write(keyBytes);
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+//        String message = "Hello, world! 小米";
+//        String keyFilePath = "./aeskey.bin";
+//        writeAesKey(keyFilePath);
+//        byte[] bytes = readAesKey(keyFilePath);
+//        DefaultDataDesensitization defaultDataDesensitization = new DefaultDataDesensitization();
+//        String encrypt = defaultDataDesensitization.encrypt(message, bytes);
+//        String decrypt = defaultDataDesensitization.decrypt(encrypt, bytes);
+//        System.out.println(decrypt);
+
+    }
+
 }
