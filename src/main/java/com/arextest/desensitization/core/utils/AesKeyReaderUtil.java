5a044451a703be2ce9e2b6a7265a2c6212ea18eb
rchen9 <rchen9@trip.com>
2024-09-12 19:27:18 +0800

    feat: read aesKey from system

--- src/main/java/com/arextest/desensitization/core/utils/AesKeyReaderUtil.java
+++ src/main/java/com/arextest/desensitization/core/utils/AesKeyReaderUtil.java
@@ -1,38 +1,24 @@
 package com.arextest.desensitization.core.utils;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.util.Base64;
 
 public class AesKeyReaderUtil {
 
-    private static final String AES_KEY_FILE_PATH = "./extension/aesKey.bin";
-    public static byte[] aesKey = null;
+  private static final String AES_KEY_FILE_PATH = "arex.desensitization.aesKey";
+  public static byte[] aesKey = null;
 
-    public static void validAesKeyFileExist() {
-        aesKey = readAesKey(AES_KEY_FILE_PATH);
+  public static void validAesKeyFileExist() {
+    aesKey = readAesKey(AES_KEY_FILE_PATH);
+  }
+
+  private static byte[] readAesKey(String keyFilePath) {
+    String aesBase64 = System.getProperty(keyFilePath);
+    byte[] keyBytes = Base64.getDecoder().decode(aesBase64);
+
+    int length = keyBytes.length;
+    if (length != 16 && length != 24 && length != 32) {
+      throw new RuntimeException("aes key file length error");
     }
-
-    private static byte[] readAesKey(String keyFilePath) {
-        byte[] keyBytes = null;
-
-        File file = new File(keyFilePath);
-        if (file.exists()) {
-            try {
-                FileInputStream fis = new FileInputStream(keyFilePath);
-                keyBytes = new byte[(int) fis.available()];
-                fis.read(keyBytes);
-                fis.close();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        } else {
-            throw new RuntimeException("aes key file not exist");
-        }
-
-        int length = keyBytes.length;
-        if (length != 16 && length != 24 && length != 32) {
-            throw new RuntimeException("aes key file length error");
-        }
-        return keyBytes;
-    }
+    return keyBytes;
+  }
 }
