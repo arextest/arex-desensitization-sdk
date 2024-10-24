c9c8aa7f5f2624fb34b931066610efdee569949d
rchen9 <rchen9@trip.com>
2024-10-24 14:06:51 +0800

    fix: get var from env

--- src/main/java/com/arextest/desensitization/core/utils/AesKeyReaderUtil.java
+++ src/main/java/com/arextest/desensitization/core/utils/AesKeyReaderUtil.java
@@ -13,11 +13,19 @@ public class AesKeyReaderUtil {
 
   private static byte[] readAesKey(String keyFilePath) {
     String aesBase64 = System.getProperty(keyFilePath);
-    byte[] keyBytes = Base64.getDecoder().decode(aesBase64);
 
+    if (aesBase64 == null || aesBase64.isEmpty()) {
+      aesBase64 = System.getenv(keyFilePath);
+    }
+
+    if (aesBase64 == null || aesBase64.isEmpty()) {
+      throw new RuntimeException("aes key not found");
+    }
+
+    byte[] keyBytes = Base64.getDecoder().decode(aesBase64);
     int length = keyBytes.length;
     if (length != 16 && length != 24 && length != 32) {
-      throw new RuntimeException("aes key file length error");
+      throw new RuntimeException("aes key length error");
     }
     return keyBytes;
   }
